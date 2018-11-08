e19576f76710a6867c771ff35e5c27e5a2d86cbc
Daniele Agueci <d.agueci1@gmail.com>
2018-11-08 10:47:56 -0500

    Template re-usable components

--- public/index.js
+++ public/index.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
+$(document).ready(() => {
     let socket = io();
 
-    socket.on('newMessage', function(data) {
-        debugger;
+    socket.on('newMessage', data => {
+        // Create new tab for first message in conversation
+        if (!$(`#sidebar #${data.sender.id}`).length) {
+            let template = $('#tab-template').html();
+
+            let html = Mustache.render(template, {
+                sender: data.sender.id,
+                lastMessage: data.message.text
+            });
+
+            $('#sidebar').append(html);
+        }
+
+        // Set last message in tab preview
+        $(`#sidebar #${data.sender.id} .last-message`).html(data.message.text);
+
+
+        let formattedTime = moment(data.timestamp / 1000).format('h:mm a');
+
+        let template = $('#message-template').html();
+
+        let html = Mustache.render(template, {
+            sender: data.sender.id,
+            timestamp: formattedTime,
+            text: data.message.text
+        });
+
+        $('.conversation').append(html);
     });
 });
