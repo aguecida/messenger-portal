8f5a12f2d64fdfcc8500f357573762889363d745
Daniele Agueci <d.agueci1@gmail.com>
2018-11-09 10:05:02 -0500

    Add message validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,6 +1,22 @@
+const crypto = require('crypto');
+
 let validate = (req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+        next();
+        return;
+    }
+
     console.log('Entering validation middleware');
-    next();
+
+    const hmac = crypto.createHmac('sha1', process.env.APP_SECRET);
+    hmac.update(req.rawBody, 'utf-8');
+
+    if (req.headers['x-hub-signature'] === `sha1=${hmac.digest('hex')}`) {
+        next();
+    }
+    else {
+        res.status(400).send('Invalid signature');
+    }
 };
 
 module.exports = {
